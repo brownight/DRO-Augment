01b7a1acf571cd7d1c0b41eb4740c81d5d29fbf3
brownight <102068025+brownight@users.noreply.github.com>
2025-05-14 22:54:47 -0400

    Update __init__.py

--- Model/lightning/data/__init__.py
+++ Model/lightning/data/__init__.py
@@ -1,10 +1,6 @@
 from .cifar import CIFAR10DataModule, CIFAR100DataModule
 from .cifar import CIFAR10CDataModule, CIFAR100CDataModule
 
-from .imagenet import ImageNetDataModule, ImageNet100DataModule
-from .imagenet import ImageNetCDataModule, ImageNet100CDataModule
-
-
 from .utils import _CC_DATASETS
 from .utils import _DATASETS
 from .utils import get_cc_dataset
